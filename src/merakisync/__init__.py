d1073109f3d1675c551dd96009d676d7390e5847
Nathan Anderson <ca.nanderson@pacs.com>
2026-04-01 21:08:21 +0000

    Refactored __init__.py files

--- src/merakisync/__init__.py
+++ src/merakisync/__init__.py
@@ -2,19 +2,19 @@
 #
 # SPDX-License-Identifier: MIT
 
-from meraki_sync.config import get_config
-from meraki_sync.dashboard import get_dashboard
-from meraki_sync.db.engine import get_engine
-from meraki_sync.cli.init import init
+from merakisync.config import get_config
+from merakisync.dashboard import get_dashboard
+from merakisync.db.engine import get_engine
+from merakisync.cli.init import init
 
 # Utils
-#from meraki_sync.utils.action_batch import create_batch_action, send_action_batches
+#from merakisync.utils.action_batch import create_batch_action, send_action_batches
 
 # Models
-from meraki_sync.models.organization import Organization
-from meraki_sync.models.network import Network
-from meraki_sync.models.device import Device
-from meraki_sync.models.alert import Alert
+from merakisync.models.organization import Organization
+from merakisync.models.network import Network
+from merakisync.models.device import Device
+from merakisync.models.alert import Alert
 
 
 # Exceptions
--- src/merakisync/utils/__init__.py
+++ src/merakisync/utils/__init__.py
@@ -1,3 +1,3 @@
 from .confirm import confirm
 from .prompt import prompt
-from meraki_sync.utils.filter_array import filter_array
+from merakisync.utils.filter_array import filter_array
